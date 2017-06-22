35038c6e9915355b13aff85a24fea8248696e9a1
Nikolay Suvorov <nsuvorov83@gmail.com>
2017-06-23 00:13:23 +0300

    Приступаю к реализации парсера, как наиболее сложной части.

---
+++ database/first_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `sources` (`id`, `short_name`, `full_name`, `url`, `spider_name`, `source_types_id`) VALUES
+(1, 'SBER', 'Сбербанк России', 'http://www.sberbank.com/ru/investor-relations/ir/news/', 'SBER1', 1);
+
+INSERT INTO `source_types` (`id`, `name`) VALUES
+(1, 'Web');
